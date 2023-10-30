5a5a934a62858c0c2673980207d038fda878f28e
Evlice <147293507+Evlice@users.noreply.github.com>
2023-10-30 01:36:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-$('.cstate').click(function(){
-  $('.drapdown > ul').slideToggle();
-})
-$('.drapdown ul li').mouseenter(function(){
-  $(this).find('.tooltip').stop().fadeIn(300).addClass('.active')
-})
-$('.drapdown ul li').mouseleave(function(){
-  $(this).find('.tooltip').stop().fadeOut(300).removeClass('.active')
-})
+/*
+Copyright 2018 benn
+Cool 3D box-shadow buttons
+v2 of Flat UI Buttons
+From my Awesome Button Bootstrap
+*/
