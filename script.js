2f25b2d601a2fb10332cea4b7e358ddded3f7ac0
Evlice <147293507+Evlice@users.noreply.github.com>
2023-10-30 01:21:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
-
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
-        }
-        
-    })
+$('.cstate').click(function(){
+  $('.drapdown > ul').slideToggle();
+})
+$('.drapdown ul li').mouseenter(function(){
+  $(this).find('.tooltip').stop().fadeIn(300).addClass('.active')
+})
+$('.drapdown ul li').mouseleave(function(){
+  $(this).find('.tooltip').stop().fadeOut(300).removeClass('.active')
 })
